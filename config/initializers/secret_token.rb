1577efe6b029cce03a815e2b7bb136d84dda1d4f
Stuart McCormack <stumac1gr@gmail.com>
2013-02-16 22:08:16 +0000

    New Project, Added Devise and Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facespace::Application.config.secret_token = '56524f99bfb87e8c5945d9ba078af679266f622039612cdf62514a19f500024a729cee380241cc28715d526a757ed74a9f62a014215605597b08e2edad701be0'
